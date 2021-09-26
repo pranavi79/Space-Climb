2c0d1b139757eb9b6bd062de81047e0aa837a0f9
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2021-09-26 19:20:12 +0530

    Updated player

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 using MLAPI;
 using MLAPI.Messaging;
 using System.Collections;
+using UnityEngine.UI;
 
-public class PlayerMovement : NetworkBehaviour {
+public class PlayerMovement : NetworkBehaviour
+{
 
     //Assingables
     public Transform playerCam;
@@ -20,13 +22,13 @@ public class PlayerMovement : NetworkBehaviour {
     public float xRotation;
     public float sensitivity = 50f;
     public float sensMultiplier = 1f;
-    
+
     //Movement
     public float moveSpeed = 4500;
     public float maxSpeed = 20;
     public bool grounded;
     public LayerMask whatIsGround;
-    
+
     public float counterMovement = 0.175f;
     private float threshold = 0.01f;
     public float maxSlopeAngle = 35f;
@@ -41,45 +43,54 @@ public class PlayerMovement : NetworkBehaviour {
     private bool readyToJump = true;
     private float jumpCooldown = 0.25f;
     public float jumpForce = 550f;
-    
+
     //Input
     float x, y;
     bool jumping, sprinting, crouching;
-    
+
     //Sliding
     private Vector3 normalVector = Vector3.up;
     private Vector3 wallNormalVector;
 
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
     }
-    
-    void Start() {
-        playerScale =  transform.localScale;
+
+    void Start()
+    {
+        playerScale = transform.localScale;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        if(!IsLocalPlayer)
+        if (!IsLocalPlayer)
         {
-            // playerCam.gameObject.GetComponent<Camera>().enabled = false;
-            playerCam.gameObject.SetActive(false);
+            // Earlier GameObject camera was disabled
+            // playerCam.gameObject.SetActive(false);
+
+            // Now MainCamera is disabled
+            playerCam.GetChild(0).GetChild(0).gameObject.SetActive(false);
         }
     }
 
-    
-    private void FixedUpdate() {
-        if(IsLocalPlayer)
+    private void FixedUpdate()
+    {
+        if (IsLocalPlayer)
         {
             Movement();
         }
         else
         {
+            // Earlier GameObject camera was disabled
+            // playerCam.gameObject.SetActive(false);
 
-            playerCam.gameObject.SetActive(false);
+            // Now MainCamera is disabled
+            playerCam.GetChild(0).GetChild(0).gameObject.SetActive(false);
         }
     }
 
-    private void Update() {
-        if(IsLocalPlayer)
+    private void Update()
+    {
+        if (IsLocalPlayer)
         {
             MyInput();
             Look();
@@ -90,12 +101,13 @@ public class PlayerMovement : NetworkBehaviour {
     /// <summary>
     /// Find user input. Should put this in its own class but im lazy
     /// </summary>
-    private void MyInput() {
+    private void MyInput()
+    {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
         jumping = Input.GetButton("Jump");
         crouching = Input.GetKey(KeyCode.LeftControl);
-      
+
         //Crouching
         if (Input.GetKeyDown(KeyCode.LeftControl))
             StartCrouch();
@@ -103,45 +115,50 @@ public class PlayerMovement : NetworkBehaviour {
             StopCrouch();
     }
 
-    private void StartCrouch() {
+    private void StartCrouch()
+    {
         transform.localScale = crouchScale;
         transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-        if (rb.velocity.magnitude > 0.5f) {
-            if (grounded) {
+        if (rb.velocity.magnitude > 0.5f)
+        {
+            if (grounded)
+            {
                 rb.AddForce(orientation.transform.forward * slideForce);
             }
         }
     }
 
-    private void StopCrouch() {
+    private void StopCrouch()
+    {
         transform.localScale = playerScale;
         transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
     }
 
-    private void Movement() {
-        FindObjectOfType<AudioManager>().Play("footsteps");
+    private void Movement()
+    {
         //Extra gravity
         rb.AddForce(Vector3.down * Time.deltaTime * 10);
-        
+
         //Find actual velocity relative to where player is looking
         Vector2 mag = FindVelRelativeToLook();
         float xMag = mag.x, yMag = mag.y;
 
         //Counteract sliding and sloppy movement
         CounterMovement(x, y, mag);
-        
+
         //If holding jump && ready to jump, then jump
         if (readyToJump && jumping) Jump();
 
         //Set max speed
         float maxSpeed = this.maxSpeed;
-        
+
         //If sliding down a ramp, add force down so player stays grounded and also builds speed
-        if (crouching && grounded && readyToJump) {
+        if (crouching && grounded && readyToJump)
+        {
             rb.AddForce(Vector3.down * Time.deltaTime * 3000);
             return;
         }
-        
+
         //If speed is larger than maxspeed, cancel out the input so you don't go over max speed
         if (x > 0 && xMag > maxSpeed) x = 0;
         if (x < 0 && xMag < -maxSpeed) x = 0;
@@ -150,13 +167,14 @@ public class PlayerMovement : NetworkBehaviour {
 
         //Some multipliers
         float multiplier = 1f, multiplierV = 1f;
-        
+
         // Movement in air
-        if (!grounded) {
+        if (!grounded)
+        {
             multiplier = 0.5f;
             multiplierV = 0.5f;
         }
-        
+
         // Movement while sliding
         if (grounded && crouching) multiplierV = 0f;
 
@@ -165,67 +183,76 @@ public class PlayerMovement : NetworkBehaviour {
         rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
     }
 
-    private void Jump() {
-        if (grounded && readyToJump) {
-            FindObjectOfType<AudioManager>().Play("jumping");
+    private void Jump()
+    {
+        if (grounded && readyToJump)
+        {
             readyToJump = false;
 
             //Add jump forces
             rb.AddForce(Vector2.up * jumpForce * 1.5f);
             rb.AddForce(normalVector * jumpForce * 0.5f);
-            
+
             //If jumping while falling, reset y velocity.
             Vector3 vel = rb.velocity;
             if (rb.velocity.y < 0.5f)
                 rb.velocity = new Vector3(vel.x, 0, vel.z);
-            else if (rb.velocity.y > 0) 
+            else if (rb.velocity.y > 0)
                 rb.velocity = new Vector3(vel.x, vel.y / 2, vel.z);
-            
+
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
-    
-    private void ResetJump() {
+
+    private void ResetJump()
+    {
         readyToJump = true;
     }
-    
+
     private float desiredX;
-    private void Look() {
+    private void Look()
+    {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
 
         //Find current look rotation
         Vector3 rot = playerCam.transform.localRotation.eulerAngles;
         desiredX = rot.y + mouseX;
-        
+
         //Rotate, and also make sure we dont over- or under-rotate.
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         //Perform the rotations
+        player.rotation = Quaternion.Euler(0, desiredX, 0);
         playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
         orientation.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
     }
 
-    private void CounterMovement(float x, float y, Vector2 mag) {
+    private void CounterMovement(float x, float y, Vector2 mag)
+    {
         if (!grounded || jumping) return;
 
         //Slow down sliding
-        if (crouching) {
+        if (crouching)
+        {
             rb.AddForce(moveSpeed * Time.deltaTime * -rb.velocity.normalized * slideCounterMovement);
             return;
         }
 
         //Counter movement
-        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0)) {
+        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
+        {
             rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
         }
-        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0)) {
+        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
+        {
             rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
         }
-        
+
         //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
-        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed) {
+        if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
+        {
             float fallspeed = rb.velocity.y;
             Vector3 n = rb.velocity.normalized * maxSpeed;
             rb.velocity = new Vector3(n.x, fallspeed, n.z);
@@ -237,7 +264,8 @@ public class PlayerMovement : NetworkBehaviour {
     /// Useful for vectors calculations regarding movement and limiting movement
     /// </summary>
     /// <returns></returns>
-    public Vector2 FindVelRelativeToLook() {
+    public Vector2 FindVelRelativeToLook()
+    {
         float lookAngle = orientation.transform.eulerAngles.y;
         float moveAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
 
@@ -247,30 +275,34 @@ public class PlayerMovement : NetworkBehaviour {
         float magnitue = rb.velocity.magnitude;
         float yMag = magnitue * Mathf.Cos(u * Mathf.Deg2Rad);
         float xMag = magnitue * Mathf.Cos(v * Mathf.Deg2Rad);
-        
+
         return new Vector2(xMag, yMag);
     }
 
-    private bool IsFloor(Vector3 v) {
+    private bool IsFloor(Vector3 v)
+    {
         float angle = Vector3.Angle(Vector3.up, v);
         return angle < maxSlopeAngle;
     }
 
     private bool cancellingGrounded;
-    
+
     /// <summary>
     /// Handle ground detection
     /// </summary>
-    private void OnCollisionStay(Collision other) {
+    private void OnCollisionStay(Collision other)
+    {
         //Make sure we are only checking for walkable layers
         int layer = other.gameObject.layer;
         if (whatIsGround != (whatIsGround | (1 << layer))) return;
 
         //Iterate through every collision in a physics update
-        for (int i = 0; i < other.contactCount; i++) {
+        for (int i = 0; i < other.contactCount; i++)
+        {
             Vector3 normal = other.contacts[i].normal;
             //FLOOR
-            if (IsFloor(normal)) {
+            if (IsFloor(normal))
+            {
                 grounded = true;
                 cancellingGrounded = false;
                 normalVector = normal;
@@ -280,19 +312,21 @@ public class PlayerMovement : NetworkBehaviour {
 
         //Invoke ground/wall cancel, since we can't check normals with CollisionExit
         float delay = 3f;
-        if (!cancellingGrounded) {
+        if (!cancellingGrounded)
+        {
             cancellingGrounded = true;
             Invoke(nameof(StopGrounded), Time.deltaTime * delay);
         }
     }
 
-    private void StopGrounded() {
+    private void StopGrounded()
+    {
         grounded = false;
     }
-    
+
     private void OutOfBonds()
     {
-        if(player.position.y < -200f)
+        if (player.position.y < -200f)
         {
             float x = -46;
             float y = -80;
@@ -314,7 +348,7 @@ public class PlayerMovement : NetworkBehaviour {
     }
 
     IEnumerator Respawn(Vector3 position)
-    {        
+    {
         yield return new WaitForSeconds(1f);
 
         player.position = position;
